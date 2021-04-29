2447a51aa04d77ba05724124d9abc4e10381aea3
Rodrigo Laguna <rlaguna@idatha.com>
2021-04-29 19:53:41 -0300

    Bug fix; improve messages

--- bot_messages.py
+++ bot_messages.py
@@ -2,10 +2,6 @@ ALT_TEXT_TUTORIAL_URL = 'https://help.twitter.com/es/using-twitter/picture-descr
 
 HELP_ARTICLE_MESSAGE = f'Este artículo podría ayudar: {ALT_TEXT_TUTORIAL_URL}'
 
-# Tweet message for friends
-AUTO_REPLY_NO_ALT_TEXT = '☝️ Este tweet sería más inclusivo con el uso de textos alternativos (alt_text) para ' \
-                         'describir  todas sus imágenes... Este artículo te podría ayudar: ' \
-                         f'{ALT_TEXT_TUTORIAL_URL}'
 # Direct message for followers
 AUTO_DM_NO_ALT_TEXT = 'Este tweet sería más inclusivo con el uso de textos alternativos (alt_text) para describir ' \
                          'todas sus imágenes... {}. Este artículo podría ayudar: ' \
@@ -17,16 +13,18 @@ AUTO_REPLY_NO_DM_NO_ALT_TEXT = '☝️ Este tweet sería más inclusivo con el u
                                f'{ALT_TEXT_TUTORIAL_URL}\n Gracias por seguirme! ' \
                                f'Mandame DM para recordarte por ahí a futuro 😉'
 
-AUTO_REPLY_NO_IMAGES_FOUND = f'☝️ Ese tweet no contiene imágenes...  Por ahora no chequeo otros contenidos (GIFs, ' \
-    f'videos, links externos).'
-
+# Reply messages to report use case
 SINGLE_USER_REPORT = '@{screen_name}: usó alt_texts en {score:.1f} % de imágenes, {n_images} analizadas'
 SINGLE_USER_NO_IMAGES_FOUND_REPORT = '@{screen_name}: no encontré tweets con imágenes.'
 HEADER_REPORT = '🔎🗒️ Aquí está tu reporte 🤓👇'  # some emojis not shown in pycharm
 FOOTER_REPORT = f'+ info acá https://rola93.github.io/altBotUY/#reportes'
 
-SINGLE_USER_NO_ALT_TEXT_QUERY = f'☝️ Ese tweet sería más inclusivo con el uso de textos alternativos (alt_text) para ' \
-    f'describir  todas sus imágenes... {HELP_ARTICLE_MESSAGE}'
+# Reply messages to @AltBotUY mentions use case
+SINGLE_USER_NO_ALT_TEXT_QUERY = '☝️ Ese tweet de @{} sería más inclusivo con el uso de textos alternativos (alt_text) ' \
+                                f'para describir  todas sus imágenes... {HELP_ARTICLE_MESSAGE}'
 
-SINGLE_USER_WITH_ALT_TEXT_QUERY = f'☝️ Ese tweet contiene textos alternativos (alt_text) para ' \
-    f'describir  todas sus imágenes 💪'
+SINGLE_USER_WITH_ALT_TEXT_QUERY = '☝️ Ese tweet de @{} contiene textos alternativos (alt_text) para describir ' \
+                                  'todas sus imágenes 💪💪💪'
+
+AUTO_REPLY_NO_IMAGES_FOUND = '☝️ Ese tweet de @{} no contiene imágenes...  Por ahora no chequeo otros contenidos ' \
+    '(GIFs, videos, links externos) 😅😬.'
